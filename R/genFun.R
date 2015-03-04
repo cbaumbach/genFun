137445e0d26b6380db9852696ffe3a51a2f2ac3e
Clemens Baumbach <clemens.baumbach@gmx.net>
2015-03-04 17:22:57 +0100

    Propagate NAs in snp data frame to output.

--- R/genFun.R
+++ R/genFun.R
@@ -7,6 +7,7 @@ find_genes <- function(d, genes, chr1 = "chr", pos = "pos", out = id,
     d[[out]] <- NA
     for (k in sort(unique(d[[chr1]]))) {
         idx <- d[[chr1]] == k
+        idx[is.na(idx)] <- FALSE
         gidx <- genes[[chr2]] == k
         if (!any(gidx)) next
         d[[out]][idx] <- match_intervals(
