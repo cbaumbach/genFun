3401b95970f924e890376e71ebfe3d157a92986f
Clemens Baumbach <clemens.baumbach@gmx.net>
2015-03-04 17:02:28 +0100

    Add failing test involving NAs for find_genes.

--- tests/testthat/test_find_genes.R
+++ tests/testthat/test_find_genes.R
@@ -28,3 +28,32 @@ rs4 3 4 NA
 
     expect_that(find_genes(d, genes), equals(dout))
 })
+
+test_that("missing values in snp data frame are propagated", {
+
+    d <- read.table(textConnection("\
+snp chr pos
+rs1 NA 1
+rs2 1 5
+rs3 2 NA
+rs4 3 4
+", "r"), header = TRUE, stringsAsFactors = FALSE)
+
+    genes <- read.table(textConnection("\
+id chr start end
+g1 1 1 3
+g2 1 1 6
+g3 2 1 4
+g4 4 1 5
+", "r"), header = TRUE, stringsAsFactors = FALSE)
+
+    dout <- read.table(textConnection("\
+snp chr pos id
+rs1 NA 1 NA
+rs2 1 5 g2
+rs3 2 NA NA
+rs4 3 4 NA
+", "r"), header = TRUE, stringsAsFactors = FALSE)
+
+    expect_that(find_genes(d, genes), equals(dout))
+})
