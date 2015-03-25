f5c6a27a1268de9a5a968c84c58d4986259a5dec
Clemens Baumbach <clemens.baumbach@gmx.net>
2015-03-25 15:47:12 +0100

    Create trees from gene ontology obo file.

--- R/go_obo.R
+++ R/go_obo.R
@@ -15,6 +15,40 @@ read.obo <- function(filename, colClasses = NULL)
     read.delim(con, colClasses = colClasses)
 }
 
+read.gene_ontology <- function(filename, ontology = c(
+                                         "biological_process",
+                                         "cellular_component",
+                                         "molecular_function"))
+{
+    d <- read.obo(filename, colClasses = "character")
+
+    ## Whenever a relationship `R' holds between two nodes `a' and
+    ## `b', i.e., whenever we have `aRb', where `R' is one of `is_a',
+    ## `part_of', `regulates', `negatively_regulates', or
+    ## `positively_regulates', we will interpret this to mean `a is a
+    ## child of b', or equivalently, `b is a parent of a'.
+    relationships <- c("is_a", "part_of", "regulates",
+                       "negatively_regulates",
+                       "positively_regulates")
+    d$parents <- apply(d[, relationships], 1L,
+                       function(x) paste(x[!is.na(x)], collapse = ","))
+
+    ## Create a separate data frame for each ontology.
+    ds <- lapply(ontology, function(x) d[d$namespace == x, ])
+    names(ds) <- ontology
+
+    ## Convert data frames into trees.
+    trees <- lapply(ds, make_tree)
+
+    ## Make trees instances of class "gene_ontology".
+    lapply(trees,
+           function(tr)
+           {
+               class(tr) <- c("gene_ontology", class(tr))
+               tr
+           })
+}
+
 dot_attributes <- function(...)
 {
     x <- list(...)
